de2bc2d8ddb39ce92e051c62a0f79e25b1957307
Kevin Gravier <kgravier@fusetg.com>
2018-10-08 15:43:53 -0400

    Finished creating a Eto.Forms version of solver and map

---
+++ LargerInteractive/LargerInteractive/DrawWidget.fs
@@ -0,0 +1,46 @@
+namespace LargerInteractive
+
+open Eto.Drawing
+open Eto.Forms
+open SimpleWorld
+
+type DrawType = Current | Path | EndPoint
+
+type DrawWidget (width: int, height: int, scale: int) as this =
+    inherit Drawable()
+
+    let positionColor (pos: SimpleWorld.Position) =
+        match pos.Type with
+        | SimpleWorld.Teleporter -> Color.FromRgb(0xFFFF00)
+        | SimpleWorld.Grass      -> Color.FromRgb(0x009900)
+        | SimpleWorld.Path       -> Color.FromRgb(0x999999)
+        | SimpleWorld.Rock       -> Color.FromRgb(0x555555)
+        | _                      -> Color.FromRgb(0x000000)
+
+    let blendColor t =
+        match t with
+        | Current -> Color.FromArgb(0x99FF0000)
+        | Path -> Color.FromArgb(0x99FF0000)
+        | EndPoint -> Color.FromArgb(0x990000FF)
+
+    let sourceBitmap = new Bitmap(width * scale, height * scale, PixelFormat.Format24bppRgb)
+    let drawHandle (g: Graphics) = g.DrawImage(sourceBitmap, float32 0.0, float32 0.0)
+
+    do
+        this.Width <- width * scale
+        this.Height <- height * scale
+        this.Paint.Add(fun e -> drawHandle e.Graphics)
+
+    member this.setPixel (x: int) (y: int) (color: Color) =
+        let xMin = x * scale
+        let yMin = y * scale
+        let xMax = xMin + scale - 1
+        let yMax = yMin + scale - 1
+        for ix in [xMin .. xMax] do
+            for iy in [yMin .. yMax] do sourceBitmap.SetPixel(ix, iy, color)
+        this.Invalidate()
+
+    member this.drawPosition (pos: SimpleWorld.Position) = this.setPixel pos.X pos.Y (positionColor pos)
+    member this.drawPositionType (pos: SimpleWorld.Position) t = this.setPixel pos.X pos.Y (Color.Blend(positionColor pos, blendColor t))
+    member this.drawAll (posList: SimpleWorld.Position list) = for pos in posList do this.drawPosition pos
+    member this.drawAllType (posList: SimpleWorld.Position list) t = for pos in posList do this.drawPositionType pos t
---
+++ LargerInteractive/LargerInteractive/State.fs
@@ -0,0 +1,81 @@
+namespace LargerInteractive
+
+open System
+open FAStar
+open SimpleWorld
+
+
+type StateStatus = New | Ticking | Solved | Waiting | Done
+type State =
+    {
+        Thoroughness: float
+        Origin: SimpleWorld.Position
+        Destination: SimpleWorld.Position
+        World: SimpleWorld.World
+        Solver: Solver<SimpleWorld.Position>
+        Status: StateStatus
+    }
+
+
+module State =
+    let random = new Random()
+
+    let rec getRandomNode() (world: SimpleWorld.World) =
+        match world.Positions.[random.Next(world.Positions.Count - 1)] with
+            | p when not (p.Type = SimpleWorld.Wall) -> p
+            | _ -> getRandomNode() world
+
+    let makeSolver() origin destination (world: SimpleWorld.World) thoroughness =
+        { Solver.create origin destination (world.neighbors) (fun t -> t.costTo) (fun t -> t.distanceTo) with
+            Thoroughness = thoroughness }
+
+    let create width height thoroughness =
+        let world = SimpleWorld.create width height
+        let origin = getRandomNode() world
+        let destination = getRandomNode() world
+        {
+            Thoroughness = thoroughness
+            Origin = origin
+            Destination = destination
+            World = world
+            Solver = makeSolver() origin destination world thoroughness
+            Status = New
+        }
+
+    let changeThoroughness thoroughness state =
+        {
+            state with
+                Thoroughness = thoroughness
+                Solver = makeSolver() state.Origin state.Destination state.World thoroughness
+                Status = New
+        }
+
+    let printUndoClosed (drawWidget: DrawWidget) state = drawWidget.drawAll (Set.toList state.Solver.ClosedNodes)
+    let printMap (drawWidget: DrawWidget) state = drawWidget.drawAll (Map.toList state.World.Positions |> List.map (fun (k,v) -> v))
+    let printEndPoints (drawWidget: DrawWidget) state =
+        drawWidget.drawPositionType state.Origin EndPoint
+        drawWidget.drawPositionType state.Destination EndPoint
+    let printCurrent (drawWidget: DrawWidget) state =
+        if not (state.Solver.CurrentNode = state.Origin) && not (state.Solver.CurrentNode = state.Destination) then drawWidget.drawPositionType state.Solver.CurrentNode Current
+    let printPath (drawWidget: DrawWidget) state =
+        if state.Solver.Status = SolverStatus.Solved then drawWidget.drawAllType state.Solver.Path Path
+
+    let tickState (drawWidget: DrawWidget) state =
+        match state.Status with
+            | StateStatus.New ->
+                printMap drawWidget state
+                printEndPoints drawWidget state
+                { state with Status = StateStatus.Waiting }
+            | StateStatus.Ticking ->
+                let newState = { state with Solver = Solver.tick state.Solver }
+                printCurrent drawWidget newState
+                match newState.Solver.Status with
+                    | SolverStatus.Open -> newState
+                    | SolverStatus.Solved -> { newState with Status = StateStatus.Solved }
+                    | _ -> raise (Exception "Failed to solve")
+            | StateStatus.Solved ->
+                printUndoClosed drawWidget state
+                printEndPoints drawWidget state
+                printPath drawWidget state
+                { state with Status = StateStatus.Waiting }
+            | _ -> state
